f8d216cd2ece311b922aae0f5e1232ce5642ba97
ankolodz <49743845+ankolodz@users.noreply.github.com>
2020-02-27 18:58:37 +0100

    Revert "."
    This reverts commit 588f0e5977489005c42259f058b9ade380ce96ef.

--- Projekt 1 Chat/Client/src/Main.java
+++ Projekt 1 Chat/Client/src/Main.java
@@ -1,18 +1,57 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class Main {
 
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException, InterruptedException {
         ClientSocket clientSocket = new ClientSocket(1);
-        ChatBox chatBox = new ChatBox(2, clientSocket);
-
-        while (true){
-            Message message = clientSocket.getMessage();
-            System.out.println(message.getTextMesssage());
+        try {
+            //ChatBox chatBox = new ChatBox(2, clientSocket);
+           // System.out.println("Client was been starting");
+            while (true) {
+               // System.out.println("Try get messaage");
+                String message = clientSocket.getMessage();
+                System.out.println(message);
+                //System.out.println("Message");
+            }
         }
-
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            if (clientSocket.isReady()){
+                clientSocket.closeConnect();
+            }
+        }
+//        System.out.println("JAVA TCP CLIENT");
+//        String hostName = "localhost";
+//        int portNumber = 11000;
+//        Socket socket = null;
+//
+//        try {
+//            // create socket
+//            socket = new Socket(hostName, portNumber);
+//
+//            // in & out streams
+//            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+//            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//
+//            // send msg, read response
+//            out.println("Ping Java Tcp");
+//            String response = in.readLine();
+//            System.out.println("received response: " + response);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (socket != null){
+//                socket.close();
+//            }
+//        }
 
     }
 }
