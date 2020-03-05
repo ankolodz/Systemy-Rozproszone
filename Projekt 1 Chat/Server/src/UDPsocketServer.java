6d77f78f91f3d14c582630f31821b90cbf9f1206
ankolodz <49743845+ankolodz@users.noreply.github.com>
2020-03-05 08:02:58 +0100

    Added easy worker and message handler

--- Projekt 1 Chat/Server/src/UDPsocketServer.java
+++ Projekt 1 Chat/Server/src/UDPsocketServer.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.util.concurrent.ExecutorService;
@@ -17,19 +19,54 @@ public class UDPsocketServer extends Thread{
     public void start(){
         try {
             serverSocket = new DatagramSocket(port);
+            byte[] incomingData = new byte[4096];
+
             threadPool = Executors.newFixedThreadPool(200);
 
-            Runnable service = () ->{
-                
+
+            while (true){
+                DatagramPacket incomingPacket = new DatagramPacket(incomingData,incomingData.length);
+                serverSocket.receive(incomingPacket);
+                byte[] data = incomingPacket.getData();
+
+                ByteArrayInputStream byteIN = new ByteArrayInputStream(data);
+                ObjectInputStream in = new ObjectInputStream(byteIN);
+
+                try{
+                    Message message = (Message) in.readObject();
+
+                    Runnable worker = new UDPworker(message,clients);
+                    threadPool.execute(worker);
+                }
+                catch (ClassNotFoundException e) {
+                    System.out.println("Błąd odebranej wiadomości");
+                }
+
             }
 
         }
         catch (SocketException e) {
             e.printStackTrace();
         }
-        finally{
+        catch (IOException e) {
+            e.printStackTrace();
+        } finally{
             if (serverSocket!= null)
                 serverSocket.close();
+        }
+    }
+    private class UDPworker implements Runnable{
+        Message message;
+        CleintsHandler clients;
+
+        public UDPworker (Message message, CleintsHandler clients) {
+            this.clients = clients;
+            this.message = message;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Do something");
         }
     }
 
